8c94eb271f87f7c65db63b5ebfec5b4801c3cd35
Karan Meghe <karanmeghe87@gmail.com>
2024-05-16 00:21:21 +0530

    add - prop-types lib has been introuduce for validating to not pass props to avoid conflict

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,5 +1,8 @@
+import PropTypes from "prop-types";
+
 const Buttons = ({
   children,
+  plain,
   primary,
   secondary,
   success,
@@ -9,6 +12,34 @@ const Buttons = ({
   outline,
 }) => {
   return <button>{children}</button>;
+};
+
+Buttons.propTypes = {
+  checkVariationValue: ({
+    plain,
+    primary,
+    secondary,
+    success,
+    danger,
+    warning,
+  }) => {
+    const count =
+      Number(!!plain) +
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!danger) +
+      Number(!!warning);
+
+    if (count > 1) {
+      return new Error(`You can pass only one value from plain,
+        primary,
+        secondary,
+        success,
+        danger,
+        warning, only one can be true`);
+    }
+  },
 };
 
 export default Buttons;
