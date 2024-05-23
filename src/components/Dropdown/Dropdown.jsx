b0517d1c19cb078c75f3e0b381e7b164a0747a34
Karan Meghe <karanmeghe87@gmail.com>
2024-05-23 16:51:58 +0530

    refactor - adding Panel component as reusable component , with consistent styling

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { GoChevronDown } from "react-icons/go";
+import { Panel } from "../index";
 
 const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,18 +30,14 @@ const Dropdown = ({ options, value, onChange }) => {
 
   return (
     <div className="w-48 relative cursor-pointer m-2">
-      <div
-        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
       >
         {value?.color || "Select..."}
         <GoChevronDown className="text-lg" />
-      </div>
-      {isOpen && (
-        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
-          {renderOptions}
-        </div>
-      )}
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderOptions}</Panel>}
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Button from "./Buttons/Buttons";
 import Accordion from "./Accordion/Accordion";
 import Dropdown from "./Dropdown/Dropdown";
+import Panel from "./Panel/Panel";
 
-export { Button, Accordion, Dropdown };
+export { Button, Accordion, Dropdown, Panel };
