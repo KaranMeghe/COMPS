6ce0dee86f9a24608d7e28f971d549711a6f32f0
Karan Meghe <karanmeghe87@gmail.com>
2024-05-29 14:27:23 +0530

    bug - small bug fixed while scrolling content before modal component get open

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,23 @@
 import ReactDOM from "react-dom";
-import Panel from "../Panel/Panel";
+import { useEffect } from "react";
 
 const Modal = ({ onClose, children, actionBar }) => {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
   return ReactDOM.createPortal(
     <article>
       <section
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></section>
 
-      <section className="absolute inset-40 p-10 bg-white">
+      <section className="fixed inset-40 p-10 bg-white">
         <div className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">{actionBar}</div>
