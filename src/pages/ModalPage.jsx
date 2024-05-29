b0236d49ac14d9a91b76f3500d0a197058a633c5
Karan Meghe <karanmeghe87@gmail.com>
2024-05-29 13:55:43 +0530

    refactor - what content get displayed on <Modal/> component will get desided by parent <ModalPage/> component

--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -11,12 +11,26 @@ const ModalPage = () => {
   const handleClose = () => {
     setShowModal(!showMoadal);
   };
+
+  const actionBar = (
+    <div>
+      <Button primary outline onClick={handleClose}>
+        I Accept
+      </Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is Terms & Conditions before you continue</p>
+    </Modal>
+  );
   return (
     <div>
       <Button primary outline onClick={handleClick}>
         Click Me
       </Button>
-      {showMoadal && <Modal onClose={handleClose} />}
+      {showMoadal && modal}
     </div>
   );
 };
